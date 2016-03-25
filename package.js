4f76b69a4cfbd59bea8b38fba57592284feaa1c8
Greg Antrim <g.antrim@kunzleigh.com>
2016-03-25 09:04:50 -0400

    version change

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
     name: 'gantrim:materialize-stylus',  // http://atmospherejs.com/materialize/materialize
     summary: 'Materialize (official): A modern responsive front-end framework based on Material Design',
-    version: '1.1.0',
+    version: '1.1.1',
     git: 'https://github.com/gantrim/meteor-materialize-stylus'
 });
 
