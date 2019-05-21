68ff21792957aff6b198b18c49368981a87d2bfd
Greg Antrim <g.antrim@kunzleigh.com>
2019-05-21 11:28:53 -0400

    update versions

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
     name: 'gantrim:materialize-stylus',
     summary: 'Materialize v1.0.0-rc.2 converted to stylus and packaged for meteor',
-    version: '2.0.2',
+    version: '2.0.3',
     git: 'https://github.com/gantrim/meteor-materialize-stylus'
 });
 
